f37e7f8b4ded02b1f7fc940c41d54930123f9d9e
yangjie <354948200@qq.com>
2026-03-17 17:02:21 +0800

    安装ssh_net库 方便上传文件 比如游戏热更新

--- obj/Debug/netstandard2.1/UnityEditorExpand.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/UnityEditorExpand.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnityEditorExpand")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55a980644cef97cde4ffb0cd0bcfc5b491dcc6b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4677d4ce7e858d334a29e1597532bb54a66b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnityEditorExpand")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnityEditorExpand")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
